766928ad7354c2a3be347f8780aaef6a1ab97b00
songshujian <kuaihujian@qq.com>
2015-12-12 18:11:44 +0800

    第二天……

---
+++ MVCDemo/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { set; get; }
+        public int Salary { get; set; }
+    }
+}
