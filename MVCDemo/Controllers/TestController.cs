df878192fd6a8a7bd8ee39c88fd9576bbe1f5844
songshujian <kuaihujian@qq.com>
2015-12-31 15:33:52 +0800

    day2 lab6

---
+++ MVCDemo/Controllers/TestController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+using MVCDemo.ViewModels;
+using MVCDemo.BusinessLayer;
+
+namespace MVCDemo.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Controller Test";
+        }
+
+        public ActionResult GetView()
+        {
+            EmployeeListViewModel employeeListViewModel = new EmployeeListViewModel();
+            EmployeeBusinessLayer empBal = new EmployeeBusinessLayer();
+            List<Employee> employees = empBal.GetEmployees();
+            List<EmployeeViewModel> empViewModels = new List<EmployeeViewModel>();
+            foreach (Employee emp in employees)
+            {
+                EmployeeViewModel empViewModel = new EmployeeViewModel();
+                empViewModel.EmployeeName = emp.FirstName + " " + emp.LastName;
+                empViewModel.Salary = emp.Salary.ToString("C");
+                if (emp.Salary > 15000)
+                {
+                    empViewModel.SalaryColor = "yellow";
+                }
+                else
+                {
+                    empViewModel.SalaryColor = "green";
+                }
+                empViewModels.Add(empViewModel);
+            }
+            employeeListViewModel.Employees = empViewModels;
+            employeeListViewModel.UserName = "admin";
+            return View("MyView", employeeListViewModel);
+        }
+    }
+}
