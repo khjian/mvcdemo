8e09c765443517495111278260cc8303de921b68
songshujian <kuaihujian@qq.com>
2016-01-21 14:26:17 +0800

    day7 lab33 卡住

---
+++ ViewModel/SPA/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MVCDemo.ViewModels;
+
+namespace ViewModel.SPA
+{
+    public class MainViewModel
+    {
+        public string UserName { get; set; }
+        public FooterViewModel FooterData { get; set; }
+    }
+}
